1bf8a4de03885404851fec7893d1bf32db5d875e
bipin <bpnsilwal12@gmail.com>
2022-03-04 12:57:47 +0545

    second-commit

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,30 @@
 import express from 'express';
+import errorHandlerMiddleware from './middleware/error-handler.js';
 import { notFoundMiddleware } from './middleware/not-found.js';
-
-
-
 const app = express();
+
+import dotenv from 'dotenv';
+
+
+dotenv.config({path:'config/config.env'})
 
 
 
 const port = process.env.PORT || 5000;
 
 
-app.get('/', (req,res)=>res.end('Welcome to my Page!!'))
+app.get('/', (req,res)=>{
+
+        throw new Error('tero tauko!!');
+        res.send('Welcome');
+
+
+});
+
+
+
 app.use( notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 
 
