09767102a5ef97ba24003733992ab2e73681b4d0
bipin <bpnsilwal12@gmail.com>
2022-03-07 20:53:35 +0545

    dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,34 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes }  from 'react-router-dom';
-import Dashboard from "./pages/Dashboard";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Error from "./pages/Error";
 import Landing from "./pages/Landing";
 import Register from "./pages/Register";
-
+import {
+  AddJob,
+  AllJob,
+  Dashboard,
+  Profile,
+  Stats,
+} from "./pages/dashboard/index";
 
 const App = () => {
   return (
     <>
-        <Router>
-            <Routes>
+      <Router>
+        <Routes>
 
-              <Route path='/' element={ <Dashboard/>  }/>
-              <Route path='/register' element={<Register/> }/>
-              <Route path='/landing' element={<Landing/>}/>
-              <Route path='*' element={ <Error/> } />
-           
+          <Route path="/" element={<Dashboard/>}>
+            <Route path="add-job" element={<AddJob />} />
+            <Route path="all-job" element={<AllJob />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="stats" element={<Stats />} />
+          </Route>
 
-
-            </Routes>
-
-
-        </Router>
-
-      
+          <Route path="/register" element={<Register />} />
+          <Route path="/landing" element={<Landing />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
     </>
   );
 };
---
+++ client/src/pages/dashboard/AddJob.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddJob = () => {
+  return (
+    <div>Add-job</div>
+  )
+}
+
+export default AddJob
